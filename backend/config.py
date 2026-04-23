b6a3b20e8c2beea4ac8cdd6bb8a7abcbaaf5fe11
sudhirsuryawanshi01 <sudhir.suryawanshi01@gmail.com>
2026-04-23 21:31:25 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,3 @@
 import os
-SUPABASE_URL = "https://twpslsfbxdocwanrlzzk.supabase.co"
-SUPABASE_KEY = "sb_publishable_f6ghFfSBvSxpJrY2A2KhTA_1ndwueot"
+SUPABASE_URL = os.getenv("https://twpslsfbxdocwanrlzzk.supabase.co")
+SUPABASE_KEY = os.getenv("sb_publishable_f6ghFfSBvSxpJrY2A2KhTA_1ndwueot")
