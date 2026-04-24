dadcb24021e49a2d99d8c7aa00fa7fecb588d552
sudhirsuryawanshi01 <sudhir.suryawanshi01@gmail.com>
2026-04-24 11:14:38 +0530

    Update login.js

--- frontend/login.js
+++ frontend/login.js
@@ -1,8 +1,10 @@
+const BASE_URL = "https://vehicle-record-management.onrender.com";
+
 async function login() {
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
 
-  let res = await fetch('/login', {
+  let res = await fetch(`${BASE_URL}/login`, {
     method: 'POST',
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({username, password})
@@ -11,7 +13,7 @@ async function login() {
   let data = await res.json();
 
   if (data.success) {
-    window.location.href = "/dashboard";
+    window.location.href = "dashboard.html"; // 🔥 important
   } else {
     document.getElementById("msg").innerText = "Invalid login";
   }
